a3c4cf3c2d42de7294feb81425804c340bfd6b60
Lovelyyoyo8 <119758674+Lovelyyoyo8@users.noreply.github.com>
2023-11-29 15:50:36 -0800

    Update socket_urllib.py

--- socket_urllib.py
+++ socket_urllib.py
@@ -1,24 +1,33 @@
 import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(("data.pr4e.org", 80))
-cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()
-mysock.send(cmd)
+mysock.settimeout(5)
+try:
+    mysock.connect(("data.pr4e.org", 80))
+    cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()
+    mysock.send(cmd)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode())
-mysock.close()
+    while True:
+        data = mysock.recv(512)
+        if len(data) < 1:
+            break
+        print(data.decode())
+except socket.error as e:
+    print(f"Socket error: {e}")
+finally:
+    mysock.close()
 
 import urllib.request, urllib.error
 
-fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
-for line in fhand:
-    print(line.decode().strip())
-
-import urllib.error
+url = "http://data.pr4e.org/romeo.txt"
+try:
+    with urllib.request.urlopen(url, timeout=5) as fhand:
+        for line in fhand:
+            print(line.decode().strip())
+except urllib.error.URLError as e:
+    print(f"URL error: {e}")
+except urllib.error.HTTPError as e:
+    print(f"HTTP error: {e}")
 
 fhand = urllib.request.urlopen("http://data.pr4e.org/romeo.txt")
 counts = dict()
